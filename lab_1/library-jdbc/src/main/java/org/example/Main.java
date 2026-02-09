1a011767a6eb12dbb985a48e1fe0946584ddb81f
Ykrll <kirillarmak7@gmail.com>
2026-02-09 09:02:21 +0300

    edited Main.java

--- lab_1/library-jdbc/src/main/java/org/example/Main.java
+++ lab_1/library-jdbc/src/main/java/org/example/Main.java
@@ -20,7 +20,7 @@ public class Main {
         StatusDao statusDao = new StatusDao();
         ReviewDao reviewDao = new ReviewDao();
 
-        System.out.println("=== LIBRARY ===");
+        System.out.println("LIBRARY\n========");
 
 
         Library library = new Library("Central Library");
@@ -31,54 +31,67 @@ public class Main {
         List<Library> libraries = libraryDao.findAll();
         System.out.println("All libraries:");
         for (Library l : libraries) {
-            System.out.println(" - " + l.getId() + " | " + l.getName());
+            System.out.println("  " + l.getId() + " | " + l.getName());
         }
 
-        System.out.println("\n=== BOOK ===");
+
+        int availableId = -1;
+
+        for (Status s : statusDao.findAll()) {
+            if ("AVAILABLE".equals(s.getName())) {
+                availableId = s.getId();
+            }
+        }
 
 
-        Book book1 = new Book("1984", "George Orwell", library.getId());
-        Book book2 = new Book("Solaris", "Stanislaw Lem", library.getId());
+        System.out.println("\nBOOK\n=====");
+
+        Book book1 = new Book("Evgeni Onegin", "Pushkin Sanya", library.getId(), availableId);
+        Book book2 = new Book("Voina i mir", "Lev Tolstui", library.getId(), availableId);
+        Book book3 = new Book("Ford Galaxy user manual", "Ymnui Chel", library.getId(), availableId);
         bookDao.save(book1);
         bookDao.save(book2);
+        bookDao.save(book3);
 
 
         List<Book> books = bookDao.findAll();
         for (Book b : books) {
-            System.out.println("Book: " + b.getTitle() + " | " + b.getAuthor());
+            System.out.println("Book: " + b.getTitle() + " | " + b.getAuthor() + " | statusId=" + b.getStatusId());
         }
 
-        System.out.println("\n=== READER ===");
+        System.out.println("\nREADER\n========");
 
-        Reader reader = new Reader("Ivan Ivanov");
+        Reader reader = new Reader("Ilon Max");
         readerDao.save(reader);
 
         for (Reader r : readerDao.findAll()) {
             System.out.println("Reader: " + r.getName());
         }
 
-        System.out.println("\n=== STATUS (reference table) ===");
 
-        for (Status s : statusDao.findAll()) {
-            System.out.println("Status: " + s.getName());
-        }
-
-        System.out.println("\n=== REVIEW (sorted by date DESC) ===");
-
+        System.out.println("\nREVIEW (sorted by date)\n========================");
 
         reviewDao.save(new Review(
                 book1.getId(),
                 reader.getId(),
                 "Great book",
-                5,
-                LocalDate.now().minusDays(1)
+                4,
+                LocalDate.now().minusDays(10)
         ));
 
         reviewDao.save(new Review(
                 book2.getId(),
                 reader.getId(),
                 "Hard but interesting",
-                4,
+                3,
+                LocalDate.now().minusMonths(3)
+        ));
+
+        reviewDao.save(new Review(
+                book3.getId(),
+                reader.getId(),
+                "Fcng awesome!",
+                5,
                 LocalDate.now()
         ));
 
@@ -92,6 +105,5 @@ public class Main {
             );
         }
 
-        System.out.println("\n=== DONE ===");
     }
 }
